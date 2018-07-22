6097cc913d01140fb4047057dbc6a2ae780fd69e
Rahul singh <rahulsinghmp968@gmail.com>
2018-07-22 17:15:38 +0530

    comparsionPrograms

---
+++ comprision/src/com/rahul/comparision/com.java
@@ -0,0 +1,24 @@
+package com.rahul.comparision;
+
+public class com {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean isy=false;
+		System.out.println(isy);
+		int age=25;
+		System.out.println(age);
+		isy=age<50;
+		System.out.println(isy);
+	
+	boolean isp=false;
+	System.out.println(isp);
+	int e=57;
+	System.out.println(e);
+	isp=e>80;
+	System.out.println(isp);
+	
+	}
+
+}
